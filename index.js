dd60e2ccdde61c5339d0c116196814f1a5c05d94
Deryn Brown <Deryn.2017@gmail.com>
2022-08-30 11:33:28 -0500

    commit tree a60f7d53f345f9259a1cf3c899cbf2023354175a

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const newCat = [...cats, name];
+    return newCat;
+}
+
+function prependCat(name) {
+    const newCat = [name, ...cats];
+    return newCat;
+}
+
+function removeLastCat() {
+    const newCat = [...cats];
+    newCat.pop();
+    return newCat;
+}
+
+function removeFirstCat() {
+    const newCat = [...cats];
+    newCat.shift();
+    return newCat;
+}
